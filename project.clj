7f9ac16b3bbaa22d0cfb4aa9966be4d0d8a9a95e
Peter Coulton <petercoulton@gmail.com>
2021-04-14 23:22:13 +0100

    docs: add readme

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cpu "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A 6502 CPU emulator"
+  :url "https://github.com/petercoulton/6502-cpu"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
